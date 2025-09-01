e7b84abc28a02fbca0dfbbd79e5f27d93d5b93f5
Tiago <beckertiago09@gmail.com>
2025-09-01 15:17:17 -0300

    email

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: "smtp.hostinger.com",
       port: 465, 
-      secure:true,
+  
       auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS, 
